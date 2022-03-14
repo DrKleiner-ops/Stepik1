118f70c2b835a9b5a8d9c13581755dfed4c9a790
DrKleiner-ops <vlad_san98@mail.ru>
2022-03-14 20:36:33 +0500

    BigInteger

--- src/Stepik.java
+++ src/Stepik.java
@@ -1,21 +1,19 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Stepik {
         public static void main(String[] args){
             Scanner in = new Scanner(System.in);
-            String text = in.nextLine();
-            text = text.replaceAll("[^a-zA-Z0-9]","");
-            String sb = new StringBuilder(text).reverse().toString();
-
-            //StringBuilder(sb).reverse.tostring(sb);
-            //System.out.println(sb);
-            if (text == sb) System.out.println("YES");
-
-
-                else System.out.println("NO");
-
-
-
-
+            int value = in.nextInt();
+            int i;
+            BigInteger k =BigInteger.ONE;
+            BigInteger result=BigInteger.valueOf(1);
+            for (i=1; i<=value; i++) {
+                result = result.multiply(BigInteger.valueOf(i));
+            
+                //po =  k.multiply(r);
+            }
+            
+            System.out.println(result);
         }}
 

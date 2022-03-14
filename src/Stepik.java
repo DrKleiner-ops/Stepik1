61d34ab6ea90fa1ee980bd15073ab2424964e4f3
DrKleiner-ops <vlad_san98@mail.ru>
2022-03-14 18:22:38 +0500

    123

---
+++ src/Stepik.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Stepik {
+        public static void main(String[] args){
+            Scanner in = new Scanner(System.in);
+            String text = in.nextLine();
+            text = text.replaceAll("[^a-zA-Z0-9]","");
+            String sb = new StringBuilder(text).reverse().toString();
+
+            //StringBuilder(sb).reverse.tostring(sb);
+            System.out.println(sb);
+            if (text == sb) System.out.println("YES");
+
+
+                else System.out.println("NO");
+
+
+
+
+        }}
+
